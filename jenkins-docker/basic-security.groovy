5e63cb0b35b527b080ea478fd88e5120ead8c079
vanshslathia <vanshslathia7051@gmail.com>
2026-05-19 01:28:34 +0530

    first

---
+++ jenkins-docker/basic-security.groovy
@@ -0,0 +1,18 @@
+#!groovy
+
+import jenkins.model.*
+import hudson.security.*
+
+def instance = Jenkins.getInstance()
+
+def hudsonRealm = new HudsonPrivateSecurityRealm(false)
+hudsonRealm.createAccount("admin", "admin123")
+
+instance.setSecurityRealm(hudsonRealm)
+
+def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
+strategy.setAllowAnonymousRead(false)
+
+instance.setAuthorizationStrategy(strategy)
+
+instance.save()
